463ba05ecb1bc26ac3786c41630be17d8d589c27
praksahb <39479839+praksahb@users.noreply.github.com>
2022-05-05 00:21:09 +0530

    added a simple top-down player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)    //positive on x-axis 
+        {
+            rigidbody2d.velocity = new Vector2(speed, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)   //negative x-axis 
+        {
+            rigidbody2d.velocity = new Vector2(-speed, 0f);
+        }
+        else if (Input.GetAxis("Vertical") > 0)   //postive y-axis
+        {
+            rigidbody2d.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)   //negative y-axis
+        {
+            rigidbody2d.velocity = new Vector2(0f, -speed);
+        }
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            rigidbody2d.velocity = new Vector2(0f,0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        {
+            Debug.Log("Level Completed");
+        }
+    }
+}
